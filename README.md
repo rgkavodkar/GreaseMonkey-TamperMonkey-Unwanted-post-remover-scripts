a710940020e37e76a7af1c24192d84a807a43593
rgkavodkar <rg.kavodkar@gmail.com>
2016-10-01 00:06:40 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+YouTube Recommendations Remover
+
+A chrome extension that removes the "Recommended channels" and "Recommended videos". 
+
+The data from YouTube server does come to your browser even after installing this extension. It simply prevents that from being displayed. 
+Looks for the right HTML tags that contains the aforementioned string(s), and sets the "display" value to "none".
+So technically, these recommendations are still there; just not displayed anymore.
+
+To add this to Chrome:
+- Download the ext folder
+- Go to Chrome > More Tools > Extensions
+- Enter into "Developer mode"
+- Click on "Load unpacked extension..."
+- Provide the location for the "ext" folder
+- Voila!
---
+++ ext/yt_rec_remover.js
@@ -0,0 +1,39 @@
+
+var ELEMENT_ID = "feed-main-what_to_watch";
+var TITLE_ID = "branded-page-module-title-text";
+
+console.log("[YouTube Recommendations Remover] Javascript loaded");
+
+// Run only for YouTube
+if(document.URL.toLowerCase().indexOf("www.youtube.com") > -1) {
+	// get the div containing the list
+	list_parent = document.getElementById(ELEMENT_ID);
+
+	if(list_parent != null) {
+		console.log("[YouTube Recommendations Remover] " + ELEMENT_ID + " found!");
+		// Get the OL object, that is the list of channels shown
+		// Get the child OL
+		ol = null;
+		for(var i = 0; i < list_parent.children.length; i++) { 
+			if(list_parent.children[i].tagName == "OL") { 
+				ol = list_parent.children[i]; 
+				break; 
+			}
+		}
+
+		// If an ordered list does exists
+		if(ol != null) {
+			// Loop through each item
+			// Check if the innerHTML contains the dreaded words
+			// If so, hide the item
+			for(var i = 0; i < ol.children.length; i++) {
+				if(ol.children[i].innerHTML.indexOf("Recommended channel for you") > -1 || ol.children[i].innerHTML.indexOf("Recommended videos") > -1) {
+					ol.children[i].style.display = "none";
+					console.log("[YouTube Recommendations Remover] Removed " + ol.children[i].getElementsByClassName(TITLE_ID)[0].innerHTML);
+				}
+			}
+		}
+	} else {
+		console.error("[YouTube Recommendations Remover] " + ELEMENT_ID + " not found!");
+	}
+}
